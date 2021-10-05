3206bff77792c37bd7e318f53ff193e115bc78ad
Jonathan Stegall <jstegall@minnpost.com>
2021-10-05 08:41:24 -0500

    flask seems to load correctly now at least

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
+import logging
+from flask import Blueprint
 from project import create_app, ext_celery
 
 app = create_app()
 celery = ext_celery.celery
 
+LOG = logging.getLogger(__name__)
+
 @app.route("/")
 def hello_world():
+    LOG.debug("Call health ok")
     return "Hello, World!"
