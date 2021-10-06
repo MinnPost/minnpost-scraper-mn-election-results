da4287e1b8d24cf962aac800fde16f3e15c6cded
Jonathan Stegall <jstegall@minnpost.com>
2021-10-06 09:36:56 -0500

    fix the log part

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
-import logging
-from flask import Blueprint
 from project import create_app, ext_celery
 
 app = create_app()
 celery = ext_celery.celery
 
-LOG = logging.getLogger(__name__)
-
 @app.route("/")
 def hello_world():
-    LOG.debug("Call health ok")
     return "Hello, World!"
