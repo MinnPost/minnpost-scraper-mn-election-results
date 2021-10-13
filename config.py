a0a51d4c8a7c49e137ef8324f56d715fba79c5ee
Jonathan Stegall <jstegall@minnpost.com>
2021-10-13 15:13:49 -0500

    install flask-caching library

--- config.py
+++ config.py
@@ -12,3 +12,9 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL", "redis://127.0.0.1:6379/0")
     RESULT_BACKEND = os.environ.get("RESULT_BACKEND", "redis://127.0.0.1:6379/0")
+    CACHE_TYPE = os.environ.get("CACHE_TYPE", "redis")
+    CACHE_REDIS_HOST = os.environ.get("CACHE_REDIS_HOST", "redis")
+    CACHE_REDIS_PORT = os.environ.get("CACHE_REDIS_PORT", 6379)
+    CACHE_REDIS_DB = os.environ.get("CACHE_REDIS_DB", 0)
+    CACHE_REDIS_URL = os.environ.get("CACHE_REDIS_URL", "redis://127.0.0.1:6379/0")
+    CACHE_DEFAULT_TIMEOUT = os.environ.get("CACHE_DEFAULT_TIMEOUT", 500)
