345ad1bcfa3c54fdcec2e9c6f1565cb323610cb9
Jonathan Stegall <jstegall@minnpost.com>
2021-11-02 15:56:10 -0500

    remove that commented line

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from src.extensions import db, celery
 from src.models import Area, Contest, Meta, Question, Result
 
 app = create_app()
-#celery = ext_celery.celery
 cli.register(app)
 
 @app.shell_context_processor
