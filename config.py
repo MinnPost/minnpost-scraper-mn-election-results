4472837f13b65f2039b3d7154e230a8298694c3e
Jonathan Stegall <jstegall@minnpost.com>
2021-11-11 10:18:59 -0600

    install celery-redbeat

--- config.py
+++ config.py
@@ -20,6 +20,7 @@ class Config(object):
     CACHE_REDIS_PORT = os.environ.get("CACHE_REDIS_PORT", 6379)
     CACHE_REDIS_DB = os.environ.get("CACHE_REDIS_DB", 0)
     CACHE_REDIS_URL = os.environ.get("CACHE_REDIS_URL", "redis://127.0.0.1:6379/0")
+    REDBEAT_REDIS_URL = os.environ.get("REDBEAT_REDIS_URL", "redis://127.0.0.1:6379/0")
     CACHE_DEFAULT_TIMEOUT = os.environ.get("CACHE_DEFAULT_TIMEOUT", 500)
     QUERY_LIST_CACHE_KEY = os.environ.get("QUERY_LIST_CACHE_KEY", "")
     ELECTION_DAY_RESULT_HOURS_START = os.environ.get("ELECTION_DAY_RESULT_HOURS_START", "")
