061791052f1e663cfc47a9220d401b7c4697bb31
Jonathan Stegall <jstegall@minnpost.com>
2022-08-11 14:48:38 -0500

    possible fix for celery so it uses the correct configuration in flask and in the worker

--- src/worker.py
+++ src/worker.py
@@ -4,35 +4,16 @@ from celery.schedules import schedule
 from redbeat import RedBeatSchedulerEntry as Entry
 
 from . import create_worker_app
+from src.celery import configure_celery
 from src.scraper.areas import scrape_areas
 from src.scraper.contests import scrape_contests
 from src.scraper.meta import scrape_meta
 from src.scraper.questions import scrape_questions
 from src.scraper.results import scrape_results
 
-def create_celery(app):
-    celery = Celery(
-        app.import_name,
-        backend=app.config["RESULT_BACKEND"],
-        broker=app.config["CELERY_BROKER_URL"],
-        redbeat_redis_url = app.config["REDBEAT_REDIS_URL"],
-    )
-    celery.conf.update(app.config)
-    TaskBase = celery.Task
-
-    class ContextTask(TaskBase):
-        abstract = True
-
-        def __call__(self, *args, **kwargs):
-            with app.app_context():
-                return TaskBase.__call__(self, *args, **kwargs)
-
-    celery.Task = ContextTask
-    return celery
-
 
 flask_app = create_worker_app()
-celery = create_celery(flask_app)
+celery = configure_celery(flask_app)
 
 
 default_interval = schedule(run_every=flask_app.config["DEFAULT_SCRAPE_FREQUENCY"])  # seconds
